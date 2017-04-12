a7f650d45e09f142649c154a07b5aea24b57245c
Amala Deshmukh <amala.d166@gmail.com>
2017-04-12 23:56:20 +0530

    Update Genetics.py
    Added Population and Species classes

--- Genetics.py
+++ Genetics.py
@@ -208,6 +208,159 @@ class Chromosome:
         return dist
 
 
+class Species:
+	def __init__(self, sid):
+		self.speciesID = sid
+		self.chromosomes = []
+		self.repID = -1
+		self.bestChromosomeID = -1
+		self.bestFitness = 0
+		self.offsprings = []
+
+	def add(self, chromosome):
+		if chromosome.fitness>self.bestFitness:
+			self.bestFitness = chromosome.fitness
+			self.bestChromosomeID = self.getSize()
+		self.chromosomes.append(chromosome)
+		chromosome.set_species(self.speciesID)
+		self.repID = np.random.randint(0, self.getSize())
+
+	def addOffspring(self, chromosome):
+		self.offsprings.append(chromosome)
+
+	def get(self, index):
+		return self.chromosomes[i]
+
+	def getSpeciesID(self):
+		return self.sid
+
+	def getSize(self):
+		return len(self.chromosomes)
+
+	def setRepresentativeID(self, index):
+		self.repID = index
+
+	def getRepresentative(self):
+		return self.chromosomes[repID]
+
+	def getAvgSpeciesFitness(self):
+		return np.sum([chromosome.fitness for chromosome in self.chromosomes])/self.getSize()
+
+	def truncatedSelect(self, pop):
+		self.chromosomes.sort(reverse=True)
+		species.chromosomes = species.chromosomes[0:int(np.ceil(self.pop*self.getSize()))]
+		species.setRepresentative(np.random.randint(0, self.getSize()))
+
+	def rouletteWheelSelect(self, pop):
+		fitness = [chromosome.fitness for chromosome in self.chromosomes]
+		total_fitness = np.sum(fitness)
+		normalized_fitness = [x/total_fitness for x in fitness]
+		self.chromosomes = np.random.choice(self.chromosomes, int(np.ceil(self.pop*self.getSize())), p=normalized_fitness)
+		species.setRepresentative(np.random.randint(0, self.getSize()))
+
+	def getRandomChromosomeID(self):
+		chromosomeID = np.random.randint(0, self.getSize())
+		return chromosomeID
+
+	def getOffsprings(self):
+		return self.offsprings
+
+	def reproduce(self, n_offsprings):
+		for i in n_offsprings:
+			x = np.random.random()
+			if x<=PROB_MUTATE:
+				parent = self.chromosomes[self.getRandomChromosomeID()]
+				mutation = np.random.choice(3, p = [PROB_ADD_EDGE, PROB_ADD_NODE, 1-(PROB_ADD_EDGE+PROB_ADD_NODE)])
+				if mutation == 0:
+					child = parent.mutate_add_edge()
+					self.addOffspring(child)
+				elif mutation == 1:
+					child = parent.mutate_add_node()
+					self.addOffspring(child)
+				else:
+					toggle = np.random.choice(2, p = [PROB_TOGGLE, 1-PROB_TOGGLE])
+					child = parent.mutate_weights()
+					if toggle==0:
+						child_t = child.mutate_toggle()
+						self.addOffspring(child_t)
+					else:
+						self.addOffspring(child)
+			else:
+				parent1 = self.chromosomes[self.getRandomChromosomeID()]
+				parent2 = self.chromosomes[self.getRandomChromosomeID()]
+				child = parent1.mutate_crossover(parent2)
+				self.addOffspring(child)
+		self.addOffspring(copy.deepcopy(self.chromosomes[bestChromosomeID]))
+
+
+class Population:
+	
+	def __init__(self, n_inputs, n_outputs):
+		self.size = INIT_SIZE
+		self.n_inputs = n_inputs
+		self.n_outputs = n_outputs
+		self.chromosomes = []
+		self.species_list = []
+		self.generation = 0
+
+	def create(self, mode=POPULATION_INIT_MODE):
+		for i in range(self.size):
+			chromosome = Chromosome(self.n_inputs, self.n_outputs)
+			if mode=="Full":
+				chromosome.make_full()
+			elif mode=="Sparse":
+				chromosome.make_sparse()
+			elif mode=="Empty":
+				chromosome.make_empty()
+			self.chromosomes.append(chromosome)
+		self.generation = 1
+
+	def evaluate(self, function):
+		for chromosome in self.chromosomes:
+			chromosome.setFitness(function(chromosome.phenotype()))
+
+	def select(self, pop, mode=SELECTION_MODE):
+		self.chromosomes = []
+		for i in range(len(self.species_list)):
+			if mode=="Truncated":
+				species.truncateSelect(REPRODUCTION_FRACTION)
+			elif mode=="RouletteWheel":
+				species.rouletteWheelSelect(REPRODUCTION_FRACTION)
+			for chromosome in self.species_list[i].chromosomes:
+				self.chromosomes.append(chromosome)
+
+	def speciate(self):
+		for chromosome in self.chromosomes:
+			if len(self.species_list) == 0 
+				species = Species(len(self.species_list))
+				species.add(chromosome)
+				self.species_list.append(species)
+			else:
+				flag = 0
+				for species in species_list:
+					representative = species.getRepresentative()
+					if chromosome.distance(representative)<DISTANCE_THRESHOLD:
+						species.add(chromosome)
+						flag = 1
+						break
+				if flag==0:
+					species = Species(len(self.species_list))
+					species.add(chromosome)
+					self.species_list.append(species)
+
+	def reproduce(self):
+		avg_species_fitness_list = [species.getAvgSpeciesFitness() for species in self.species_list]
+		total_avg_species_fitness = np.sum(avg_species_fitness_list)
+		self.chromosomes = []
+		for species,avg_species_fitness in zip(self.species_list, avg_species_fitness_list):
+			n_offsprings = self.size*avg_species_fitness/total_avg_species_fitness
+			species.reproduce(n_offsprings)
+			self.chromosomes = self.chromosomes + species.getOffsprings()
+		self.species = []
+
+	def incrementGeneration(self):
+		self.generation = self.generation + 1
+
 
 x=Chromosome(2, 2)
 x.make_full()
