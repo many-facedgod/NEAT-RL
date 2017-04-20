4f1e638264b359bfde032a5651850424cf97125b
Tanmaya Dabral <tanmayadabral@gmail.com>
2017-04-20 20:18:50 +0530

    Formatting fixes

--- Population.py
+++ Population.py
@@ -60,25 +60,25 @@ class Population:
     def reproduce(self):
         avg_species_fitness_list = [species.getAvgSpeciesFitness() for species in self.species_list]
         total_avg_species_fitness = np.sum(avg_species_fitness_list)
-		delta_parameter = 0
-		if DELTA_PARAMETER == "Best":
-			delta_parameter = self.getBestChromosome().fitness
+        delta_parameter = 0
+        if DELTA_PARAMETER == "Best":
+            delta_parameter = self.getBestChromosome().fitness
         self.chromosomes = []
         for species, avg_species_fitness in zip(self.species_list, avg_species_fitness_list):
             n_offsprings = int(self.size * avg_species_fitness / total_avg_species_fitness)
             species.reproduce(n_offsprings)
             self.chromosomes = self.chromosomes + species.getOffsprings()
-	    	if DELTA_PARAMETER = "Best" and delta_parameter < species.getBestChromosome().fitness:
-				delta_parameter = species.getBestChromosome().fitness
-		if DELTA_PARAMETER = "Avg":
-			delta_parameter = self.getAverageFitness()
-		if delta_parameter > self.bestperformance:
-			self.bestperformance = delta_parameter
-			self.n_deltagen = 0
-		else:
-			self.n_deltagen += 1
-		if self.n_deltagen == DELTA_GENERATIONS and len(self.species_list)>1: #Add to config
-			self.chromosome = self.species_list.sort(reverse=True)[:2]
+            if DELTA_PARAMETER == "Best" and delta_parameter < species.getBestChromosome().fitness:
+                delta_parameter = species.getBestChromosome().fitness
+        if DELTA_PARAMETER == "Avg":
+            delta_parameter = self.getAverageFitness()
+        if delta_parameter > self.bestperformance:
+            self.bestperformance = delta_parameter
+            self.n_deltagen = 0
+        else:
+            self.n_deltagen += 1
+        if self.n_deltagen == DELTA_GENERATIONS and len(self.species_list)>1:
+            self.chromosomes = self.species_list.sort(reverse=True)[:2]
         self.species_list = []
 
     def incrementGeneration(self):
